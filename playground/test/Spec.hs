cd469eca23d1b0f6636c5687ae437f7ad02404b2
George Spanos <gspanos@moby-it.com>
2024-06-05 20:13:03 +0300

    transfomers

--- playground/test/Spec.hs
+++ playground/test/Spec.hs
@@ -1,5 +1,5 @@
 import qualified LearnApplicativeSpec as LA (main)
-import qualified LearnFunctorsSpec as LF (main)
+-- import qualified LearnFunctorsSpec as LF (main)
 import Test.Hspec (hspec)
 
 main :: IO ()
